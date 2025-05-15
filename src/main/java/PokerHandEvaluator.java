db2dc1d04c1c1299942155aa3259e6f5ef9db513
MarkusZapf <Markus.Zapf@gmx.de>
2025-05-15 18:31:26 +0200

    feat: Add evaluation for PokerHand rank as Straight

--- src/main/java/PokerHandEvaluator.java
+++ src/main/java/PokerHandEvaluator.java
@@ -6,11 +6,23 @@ public class PokerHandEvaluator {
 
         if(isFlush(pokerCards)){
             return Rank.FLUSH;
+        } else if(isStraight(pokerCards)){
+            return Rank.STRAIGHT;
         }
         return Rank.HIGH_CARD;
     }
 
     public static boolean isFlush (List<PokerCard> pokerCards) {
         return pokerCards.stream().map(PokerCard::getSuit).distinct().count() == 1;
+    }
+
+    public static boolean isStraight(List<PokerCard> pokerCards) {
+        List<Integer> sortedOrdinalRanks = pokerCards.stream()
+                .map(pokerCard -> pokerCard.getValue().ordinal()).sorted().toList();
+        for(int i = 0; i < sortedOrdinalRanks.size() - 1; i++){
+            if(sortedOrdinalRanks.get(i) + 1 != sortedOrdinalRanks.get(i+1)){
+                return false;
+            }
+        }  return true;
     }
 }
