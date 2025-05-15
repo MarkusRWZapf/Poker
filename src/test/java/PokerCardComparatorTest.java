69b23d28ba0c0e8925b499d4ee74203707cfaaae
MarkusZapf <Markus.Zapf@gmx.de>
2025-05-15 14:40:57 +0200

    test: Add failing test for sorting PokerCards (no implementation yet)

---
+++ src/test/java/PokerCardComparatorTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PokerCardComparatorTest {
+
+    private final List<PokerCard> pokerCards = new ArrayList<>();
+
+    @BeforeEach
+    public void setUp() {
+        pokerCards.add(new PokerCard(Suit.CLUBS, Rank.FIVE));
+        pokerCards.add(new PokerCard(Suit.SPADES, Rank.KING));
+        pokerCards.add(new PokerCard(Suit.HEARTS, Rank.THREE));
+        pokerCards.add(new PokerCard(Suit.CLUBS, Rank.ACE));
+        pokerCards.add(new PokerCard(Suit.DIAMONDS, Rank.TEN));
+    }
+
+    @Test
+    public void listIsSortedAccordingToPokerCardRanking() {
+        assertEquals(new PokerCard(Suit.HEARTS, Rank.THREE), pokerCards.getFirst());
+        assertEquals(new PokerCard(Suit.CLUBS, Rank.FIVE), pokerCards.get(1));
+        assertEquals(new PokerCard(Suit.DIAMONDS, Rank.TEN), pokerCards.get(2));
+        assertEquals(new PokerCard(Suit.SPADES, Rank.KING), pokerCards.get(3));
+        assertEquals(new PokerCard(Suit.CLUBS, Rank.ACE), pokerCards.getLast());
+    }
+
+
+}
