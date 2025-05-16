d1fbe24afe9a55c7814c6a3f6799755b5850553a
MarkusZapf <Markus.Zapf@gmx.de>
2025-05-16 10:52:30 +0200

    feat: Add evaluation for PokerHand as one_pair, three_of_a_kind and four_of_a_kind rank

--- src/main/java/PokerHandEvaluator.java
+++ src/main/java/PokerHandEvaluator.java
@@ -1,6 +1,7 @@
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PokerHandEvaluator {
 
@@ -8,11 +9,15 @@ public class PokerHandEvaluator {
 
         boolean isFlush = isFlush(pokerCards);
         boolean isStraight = isStraight(pokerCards);
+        Map<Value, Long> valueOccurences = countValueOccurencesInHand(pokerCards);
 
         Map<Rank, Boolean> rankingMap = new LinkedHashMap<>();
         rankingMap.put(Rank.STRAIGHT_FLUSH, isFlush && isStraight);
+        rankingMap.put(Rank.FOUR_OF_A_KIND, valueOccurences.containsValue(4L));
         rankingMap.put(Rank.FLUSH, isFlush);
         rankingMap.put(Rank.STRAIGHT, isStraight);
+        rankingMap.put(Rank.THREE_OF_A_KIND, valueOccurences.containsValue(3L));
+        rankingMap.put(Rank.ONE_PAIR, valueOccurences.containsValue(2L));
 
         return rankingMap.entrySet().stream()
                 .filter(Map.Entry::getValue)
@@ -29,9 +34,13 @@ public class PokerHandEvaluator {
         List<Integer> orderedRankIndices = pokerCards.stream()
                 .map(pokerCard -> pokerCard.getValue().ordinal()).sorted().toList();
         for(int i = 0; i < orderedRankIndices.size() - 1; i++){
-            if(orderedRankIndices.get(i) + 1 != orderedRankIndices.get(i+1)){
+            if(orderedRankIndices.get(i) + 1 != orderedRankIndices.get(i + 1)){
                 return false;
             }
         }  return true;
+    }
+
+    public static Map<Value, Long> countValueOccurencesInHand(List<PokerCard> pokerCards) {
+        return pokerCards.stream().collect(Collectors.groupingBy(PokerCard::getValue, Collectors.counting()));
     }
 }
