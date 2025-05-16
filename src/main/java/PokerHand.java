c0708b0910c52f1013cfe7f51459ba450417cbc4
MarkusZapf <Markus.Zapf@gmx.de>
2025-05-16 15:38:56 +0200

    refactor: Move evaluation logic into PokerHand

--- src/main/java/PokerHand.java
+++ src/main/java/PokerHand.java
@@ -2,7 +2,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -14,7 +15,48 @@ public class PokerHand {
 
     public PokerHand(List<PokerCard> cards) {
         this.cards = cards;
-        this.rank = PokerHandEvaluator.rankCardsInHand(cards);
+        this.rank = rankCardsInHand(cards);
+    }
+
+    public Rank rankCardsInHand(List<PokerCard> pokerCards) {
+
+        boolean isFlush = isFlush(pokerCards);
+        boolean isStraight = isStraight(pokerCards);
+        Map<Value, Long> valueOccurences = countValueOccurencesInHand(pokerCards);
+
+        Map<Rank, Boolean> rankingMap = new LinkedHashMap<>();
+        rankingMap.put(Rank.STRAIGHT_FLUSH, isFlush && isStraight);
+        rankingMap.put(Rank.FOUR_OF_A_KIND, valueOccurences.containsValue(4L));
+        rankingMap.put(Rank.FULL_HOUSE, valueOccurences.containsValue(3L) && valueOccurences.containsValue(2L));
+        rankingMap.put(Rank.FLUSH, isFlush);
+        rankingMap.put(Rank.STRAIGHT, isStraight);
+        rankingMap.put(Rank.THREE_OF_A_KIND, valueOccurences.containsValue(3L));
+        rankingMap.put(Rank.TWO_PAIR, Collections.frequency(new ArrayList<>(valueOccurences.values()), 2L) == 2);
+        rankingMap.put(Rank.ONE_PAIR, valueOccurences.containsValue(2L));
+
+        return rankingMap.entrySet().stream()
+                .filter(Map.Entry::getValue)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(Rank.HIGH_CARD);
+    }
+
+    private boolean isFlush (List<PokerCard> pokerCards) {
+        return pokerCards.stream().map(PokerCard::getSuit).distinct().count() == 1;
+    }
+
+    private boolean isStraight(List<PokerCard> pokerCards) {
+        List<Integer> orderedRankIndices = pokerCards.stream()
+                .map(pokerCard -> pokerCard.getValue().ordinal()).sorted().toList();
+        for(int i = 0; i < orderedRankIndices.size() - 1; i++){
+            if(orderedRankIndices.get(i) + 1 != orderedRankIndices.get(i + 1)){
+                return false;
+            }
+        }  return true;
+    }
+
+    private Map<Value, Long> countValueOccurencesInHand(List<PokerCard> pokerCards) {
+        return pokerCards.stream().collect(Collectors.groupingBy(PokerCard::getValue, Collectors.counting()));
     }
 
 }
--- src/test/java/PokerHandTest.java
+++ src/test/java/PokerHandTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PokerHandEvaluatorTest {
+public class PokerHandTest {
 
     private List<PokerCard> pokerCards = new ArrayList<>();
 
